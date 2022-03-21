a8882b54e316f5c1f901713fcfbf70436f9b2c28
Katarischa <bichina.ma@phystech.edu>
2022-03-21 20:17:24 +0300

    phonebook_done

--- work_2/phone_search.cpp
+++ work_2/phone_search.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "phonebook.h"
-#include "phonebook.cpp"
 
 
 int main(){
@@ -8,17 +7,37 @@ int main(){
     std::string surname = "Nikelson";
     Phonebook phonebook;
 
+    // adding new numbers
     phonebook.New_number(number,surname);
     phonebook.New_number(89114039478, "Abramovich");
+    phonebook.New_number(80677822893, "Sunny");
+    phonebook.New_number(86060309021, "Elly");
+    phonebook.New_number(84317571434, "Donald");
+    phonebook.New_number(85363805564, "Ivanov");
+    phonebook.New_number(82162939183, "Zendy");
+    phonebook.New_number(85641003468, "Kennedy");
+    phonebook.New_number(83672678589, "Obama");
+    phonebook.New_number(87568921564, "Hilton");
+
+    std::cout << "-----------------Search Number in Phonebook-----------------------" << std::endl;
 
     std::cout << phonebook.Search_number("Nikelson")<< std::endl;
     std::cout << phonebook.Search_number("Mikelson")<< std::endl;
+    std::cout << phonebook.Search_number("Kennedy")<< std::endl;
 
-    auto a = phonebook.No_number(0);
-    std::cout << a.first <<" "<< a.second << std::endl;
+    std::cout << "-----------------Get Number by Index------------------------------" << std::endl;
 
-    auto t = phonebook.Random_number();
-    std::cout << t.first <<" "<< t.second << std::endl;
+    for (auto i = 0u; i < 10u; i++) {
+        auto get = phonebook.Get_index_number(i);
+        std::cout << get.first <<" "<< get.second << std::endl;}
+
+    std::cout << "------------------Output Random Numbers----------------------------" << std::endl;
+
+    for (auto i = 0u; i < 10u; i++) {
+        auto random = phonebook.Random_number();
+        std::cout << random.first <<" "<< random.second << std::endl;}
+
+    std::cout << "-------------------------------------------------------------------" << std::endl;
 
     return 0;
 }
--- work_2/phonebook.h
+++ work_2/phonebook.h
@@ -5,6 +5,50 @@
 #ifndef IV_TERM_1_PHONEBOOK_H
 #define IV_TERM_1_PHONEBOOK_H
 
+#include <iostream>
+#include "chrono"
+#include <map>
+#include "phonebook.h"
+#include "random"
+
+class Phonebook{
+public:
+    Phonebook() = default;
+
+    void New_number(long long number, std::string surname){
+        phonebook.emplace(surname, number);
+    };
+
+    long long Search_number(std::string surname){
+        try {
+            return phonebook.at(surname);
+        }
+        catch ( const std::out_of_range& e){
+            std::cerr << e.what() <<" Surname "<< surname<<" isn't in phonebook, please, try again" <<  std::endl;
+            return 0;
+        }
+    };
+
+    auto Get_index_number(int no){
+        return *std::next(phonebook.begin(), no);
+    };
+
+    auto Random_number(){
+        auto curr_time = std::chrono::steady_clock::now().time_since_epoch().count();
+        auto generator = std::mt19937_64(curr_time);
+        std::uniform_int_distribution<> distribution;
+        int a = distribution(generator);
+        a %= std::size(phonebook);
+        return Get_index_number(a);
+    };
+
+    ~Phonebook() = default;
+private:
+    std::map<std::string, long long> phonebook;
+
+};
+
+
 
 
 #endif //IV_TERM_1_PHONEBOOK_H
